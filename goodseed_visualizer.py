a6bc2ce72202103904a9b329e21866feb8d24caa
Maksym Khavil <mkhavil@gmail.com>
2026-03-04 13:13:48 +0100

    Save PoC changes

---
+++ goodseed_visualizer.py
@@ -0,0 +1,102 @@
+import goodseed
+import numpy as np
+from collections import OrderedDict
+from monitorch.visualizer import AbstractVisualizer, PlayerVisualizer, PrintVisualizer
+from typing import Any
+
+
+class GoodSeedVisualizer(AbstractVisualizer):
+    def __init__(
+        self,
+        *,
+        hp_dicts: dict[str, Any] | None = None,
+        prefix: str = "train/monitorch",
+        **kwargs,
+    ):
+        self.run = goodseed.Run(**kwargs)
+        self.prefix = prefix
+        print(hp_dicts)
+        hp_dicts = hp_dicts if hp_dicts is not None else {}
+        print(hp_dicts)
+        for name, value in hp_dicts.items():
+            self.run[name] = value
+
+    def close(self):
+        self.run.close()
+
+    def __del__(self):
+        self.close()
+
+    def plot_numerical_values(
+        self,
+        epoch: int,
+        main_tag: str,
+        values_dict: OrderedDict[str, dict[str, float]],
+        ranges_dict: OrderedDict[str, dict[tuple[str, str], tuple[float, float]]]
+        | None = None,
+    ) -> None:
+        main_prefix = self.prefix + "/" + main_tag
+
+        for value_name, value_dict in values_dict.items():
+            value_prefix = main_prefix + "/" + value_name
+            for stat_name, stat in value_dict.items():
+                stat = stat if np.isfinite(stat) else 0
+                self.run[value_prefix + "/" + stat_name].log(stat, step=epoch)
+
+        for range_name, range_dict in ranges_dict.items():
+            range_prefix = main_prefix + "/" + range_name
+            for stat_name, stat in range_dict.items():
+                self.run[range_prefix + "/" + stat_name[0]].log(
+                    stat[0] if np.isfinite(stat[0]) else 0, step=epoch
+                )
+                self.run[range_prefix + "/" + stat_name[1]].log(
+                    stat[1] if np.isfinite(stat[1]) else 0, step=epoch
+                )
+
+    def plot_probabilities(
+        self,
+        epoch: int,
+        main_tag: str,
+        values_dict: OrderedDict[str, dict[str, float]],
+    ) -> None:
+        main_prefix = self.prefix + "/" + main_tag
+
+        for value_name, value_dict in values_dict.items():
+            value_prefix = main_prefix + "/" + value_name
+            for stat_name, stat in value_dict.items():
+                stat = stat if np.isfinite(stat) else 0
+                self.run[value_prefix + "/" + stat_name].log(stat, step=epoch)
+
+    def plot_relations(
+        self,
+        epoch: int,
+        main_tag,
+        values_dict: OrderedDict[str, dict[str, float]],
+    ) -> None:
+        # not implementing relations as they are extremely ugly without stack plot
+        pass
+
+    def register_tags(self, main_tag: str, tag_attr) -> None:
+        """
+        Prepare visualizer's inner state for plot.
+
+        Parameters
+        ----------
+        main_tag : str
+            Name of the collection of plots.
+        tag_attr : TagAttributes
+            Tag attributes to configure plot.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    vis = GoodSeedVisualizer(
+        hp_dicts={"moments": "default", "lr": 3e-2},
+        run_id="OPT-02",
+    )
+    # vis = PrintVisualizer()
+    player = PlayerVisualizer(
+        "experiments/adamw_basic_showcase/logs/adam_lr3e-2_wd0.05_moments-default.pkl",
+        vis,
+    ).playback()
---
+++ notes4matej.md
@@ -0,0 +1,3 @@
+* passing None/null/nan panics SQL and the whole run is stopped, not commmon, though not desired
+* the link yielded by `goodseed serve` (https://goodseed.ai/app/local?port=8765) is 404-ed
+* I los OPT-00 ???, did not manage to pinpoint exact moment, if i understand correctly is result of serving
