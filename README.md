d6c2c4b639e71ee95f268726b51e4dd1776686b4
Maksym Khavil <mkhavil@gmail.com>
2026-03-05 15:20:27 +0100

    Setup kaggle infra

---
+++ README.md
@@ -0,0 +1,63 @@
+# Monitorch Experiments
+
+## Setup
+
+```bash
+uv sync
+source .venv/bin/activate
+```
+
+If you want to run experiments on kaggle or google-colab you need to set authentication variables `.env`.
+
+## Usage
+
+Scripts can be run on locally, on kaggle and google colab. To run locally just execute your script with python from venv
+
+```bash
+ptyhon path/to/your/script.py
+```
+
+### Kaggle
+
+First of all you need to setup your kaggle API token and put it into `.env`.
+
+Second you must create directory for your experiment and initialize it with `kernel-metadata.json`
+
+```
+mkdir -p path/to/experiment
+dotenv run kaggle kernel init -p path/to/experiment
+```
+
+After you have configured kernel metadata and experiment script is prepared you can run the experiment.
+
+```
+dotenv run kaggle kernel push -p path/to/experiment [--accelerator "NvidiaTeslaP100"/"NvidiaTeslaT4"/"TpuV6E8"]
+```
+
+To check on the status use:
+
+```
+dotenv run kaggle kernel status your-username/name-of-experiment
+```
+
+To download output:
+
+```
+mkdir -p path/to/experiment/output
+dotenv run kaggle kernels output your-username/name-of-experiment -p path/to/experiment/output
+```
+
+### Google Colab
+ 
+TODO
+
+## Project structure
+
+```
+.
+├── benchmark/         # directory for benchmarks and time tests
+├── experiments/       # directory for deep-learning experiments
+├── LICENSE
+├── pyproject.toml
+└── README.md
+```
---
+++ experiments/test/test.py
@@ -0,0 +1,77 @@
+import os
+import sys
+
+from collections import OrderedDict
+
+# Install your required packages
+status = os.system("pip install monitorch")
+
+import torch  # noqa: E402
+import torch.nn as nn  # noqa: E402
+from monitorch.inspector import PyTorchInspector  # noqa: E402
+from monitorch.lens import (  # noqa: E402
+    LossMetrics,
+    ParameterGradientActivation,
+    ParameterUpdateGeometry,
+)
+from monitorch.visualizer import (  # noqa: E402
+    RecorderVisualizer,
+    MatplotlibVisualizer,
+    PlayerVisualizer,
+)
+from tqdm import trange  # noqa: E402
+
+print(f"GPU Available: {torch.cuda.is_available()}")
+if torch.cuda.is_available():
+    print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+
+classificator = nn.Sequential(
+    OrderedDict(
+        [
+            ("lin1", nn.Linear(10, 10)),
+            ("relu", nn.ReLU()),
+            ("lin2", nn.Linear(10, 2)),
+        ]
+    )
+)
+
+if (
+    current_device := torch.accelerator.current_accelerator(check_available=True)
+) is not None:
+    classificator.to(current_device)
+
+optimizer = torch.optim.AdamW(classificator.parameters())
+loss_fn = nn.CrossEntropyLoss()
+
+inspector = PyTorchInspector(
+    module=classificator,
+    lenses=[
+        LossMetrics(loss_fn=loss_fn),
+        ParameterGradientActivation(),
+        ParameterUpdateGeometry(optimizer),
+    ],
+    visualizer=RecorderVisualizer("log.pkl"),
+)
+
+for epoch in trange(20):
+    for batch in range(1024):
+        x = torch.normal(mean=0, std=1, size=(256, 10), dtype=torch.float32)
+        y = ((x * x).sum(dim=1) < 1).to(torch.int64)
+
+        pred = classificator(x)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    inspector.tick()
+
+# effective way to close log file
+del inspector
+
+player = PlayerVisualizer("log.pkl", MatplotlibVisualizer()).playback()
+
+fig = player.visualizer.show_fig()
+
+fig.savefig("mpl_visualization.png")
